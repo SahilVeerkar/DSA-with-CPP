9ba12c719904df672744ad6e2ac532c5c12d6dca
user.name:"sahil veerkar <user.email:"veerkarsahil@gmail.com>
2026-02-24 12:41:35 +0530

    lecture-1 of DSA with CPP

---
+++ (1)lecture/datatype.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //data types int ,char,float,double,bool **bool is use for true and false//
+    int a=3; 
+    char ch='a';
+    float fr=1.2;
+    double db=1.23;
+    bool bl=true;
+   // int size is use for cheack memory size //
+      int size=sizeof(a);
+    cout<<a<<"\n"<<ch<<"\n"<<fr<<"\n"<<db<<"\n"<<bl<<"\n";
+    cout<<size;
+}
