60059dee7d366d10ab3b96999d6a95592d43ece6
user.name:"sahil veerkar <user.email:"veerkarsahil@gmail.com>
2026-03-03 18:34:38 +0530

    decimal to binary convertion

---
+++ (9)number_system/decimalToBinary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int dec = 10;
+  int ans = 0;
+  ;
+
+  int i = 0;
+  while (dec != 0)
+  {
+    int rem = dec % 2;
+    ans = (rem * pow(10, i)) + ans;
+    i++;
+    dec = dec / 2;
+  }
+  cout << ans;
+}
