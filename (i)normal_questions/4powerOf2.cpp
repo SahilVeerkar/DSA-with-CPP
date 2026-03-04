4978d37758c6f7d6cc3f6b62f827e28940e74fc1
Sahil <veerkarsahil@gmail.com>
2026-03-04 07:45:35 +0530

    problem no. 231 of leetcode(power of 2)

---
+++ (i)normal_questions/4powerOf2.cpp
@@ -0,0 +1,46 @@
+// 231. Power of Two (leetcode)
+
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+// Example 1:
+
+// Input: n = 1
+// Output: true
+// Explanation: 20 = 1
+// Example 2:
+
+// Input: n = 16
+// Output: true
+// Explanation: 24 = 16
+// Example 3:
+
+// Input: n = 3
+// Output: false
+
+// Constraints:
+
+// -231 <= n <= 231 - 1
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  int x;
+  cin >> x;
+  int ans;
+  for (int i = 0; i < 31; i++)
+  {
+    ans = pow(2, i);
+    if (ans == x)
+    {
+      cout << "true";
+    }
+  }
+  if (ans != x)
+  {
+    cout << "false";
+  }
+}
