2a78b563f696335eebcb3bba773e214dc9e505c4
Sahil <veerkarsahil@gmail.com>
2026-03-16 13:49:44 +0530

    pushbackexample

---
+++ (m)vector/2pushback.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+  vector<int> a;
+  a.push_back(4);
+  a.push_back(8);
+  a.push_back(7);
+
+  for (int i = 0; i < a.size(); i++)
+  {
+    cout << a[i];
+  }
+}
