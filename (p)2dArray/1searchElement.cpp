e7e9d45060677c67c03de75a7323409d1252fc0e
Sahil <veerkarsahil@gmail.com>
2026-03-19 17:12:49 +0530

    search element in array

---
+++ (p)2dArray/1searchElement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int arr[4][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  int row = 4;
+  int col = 3;
+  int x = 7;
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      if (arr[i][j] == x)
+      {
+        cout << "[" << i << "]";
+        cout << "[" << j << "]";
+      }
+    }
+  }
+}
