405c221cfa1c7ea00ae5d35dadb246d36043df5b
Sahil <veerkarsahil@gmail.com>
2026-03-30 02:00:02 +0530

    problem no. 12 of leetcode (integer to roman)

---
+++ (q)string/18intToRoman.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+  string intToRoman(int num)
+  {
+    string result = "";
+    vector<int> value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    for (int i = 0; i < value.size(); i++)
+    {
+      while (num >= value[i])
+      {
+        result += symbol[i];
+        num -= value[i];
+      }
+    }
+    return result;
+  }
+};
